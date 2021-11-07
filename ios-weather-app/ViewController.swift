b9ddfa24db83e9fab6a70b1281a1bec7520df8d3
JIINHEO <gjwldls123@gmail.com>
2021-11-07 18:07:49 +0900

    Test: Test

--- ios-weather-app/ViewController.swift
+++ ios-weather-app/ViewController.swift
@@ -11,9 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test")
         // Do any additional setup after loading the view.
     }
-
-
 }
 
